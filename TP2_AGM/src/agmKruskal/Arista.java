2fbe8002f1558f943153addb5e761ea44241079e
Naim21 <2002natorres@gmail.com>
2024-05-08 19:10:25 -0300

    a

---
+++ TP2_AGM/src/agmKruskal/Arista.java
@@ -0,0 +1,32 @@
+package agmKruskal;
+
+public class Arista implements Comparable<Arista> {
+	
+	private int origen;
+	  private int destino;
+	  private int peso;
+
+	  public Arista(int origen, int destino, int peso) {
+	    this.origen = origen;
+	    this.destino = destino;
+	    this.peso = peso;
+	  }
+
+	  public int getOrigen() {
+	    return origen;
+	  }
+
+	  public int getDestino() {
+	    return destino;
+	  }
+
+	  public int getPeso() {
+	    return peso;
+	  }
+
+	  @Override
+	  public int compareTo(Arista otraArista) {
+	    return Integer.compare(this.peso, otraArista.getPeso());
+	  }
+
+}
---
+++ TP2_AGM/src/agmKruskal/Grafo.java
@@ -0,0 +1,78 @@
+package agmKruskal;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Grafo {
+
+	private int[][] matrizDePesos;
+	private int numVertices;
+
+	public Grafo(int vertices) {
+		this.matrizDePesos = new int[vertices][vertices];
+		this.numVertices = matrizDePesos.length;
+	}
+
+	public void agregarAristaConPeso(int i, int j, int peso) {
+		this.matrizDePesos[i][j] = peso;
+		this.matrizDePesos[j][i] = peso;
+	}
+
+	public List<Arista> obtenerTodasLasAristas() {
+		List<Arista> aristas = new ArrayList<>();
+		for (int i = 0; i < numVertices; i++) {
+			for (int j = i + 1; j < numVertices; j++) {
+				if (matrizDePesos[i][j] != 0) {
+					Arista arista = new Arista(i, j, matrizDePesos[i][j]);
+					aristas.add(arista);
+				}
+			}
+		}
+		return aristas;
+	}
+
+	public List<Arista> kruskal() {
+		List<Arista> aristas = obtenerTodasLasAristas();
+		Collections.sort(aristas); //Ordena de las aristas de menor a mayor segun su peso
+
+		UnionFind unionFind = new UnionFind(this.numVertices); //Se crean las componentes conexas iniciales
+
+		List<Arista> agm = new ArrayList<>();
+		for (Arista arista : aristas) {
+			int origen = arista.getOrigen();
+			int destino = arista.getDestino();
+			if (!unionFind.find(origen, destino)) { //Estan en la misma componente conexa?
+				agm.add(arista);
+				unionFind.union(origen, destino); //Apuntar a la misma raiz
+			}
+		}
+		return agm; //Lista de aristas del arbol generador minimo
+	}
+
+	public static void main(String[] args) {
+
+		Grafo miGrafo = new Grafo(9);
+		miGrafo.agregarAristaConPeso(0, 1, 4);
+		miGrafo.agregarAristaConPeso(0, 7, 8);
+		miGrafo.agregarAristaConPeso(1, 2, 8);
+		miGrafo.agregarAristaConPeso(1, 7, 12);
+		miGrafo.agregarAristaConPeso(2, 8, 3);
+		miGrafo.agregarAristaConPeso(2, 3, 6);
+		miGrafo.agregarAristaConPeso(2, 5, 4);
+		miGrafo.agregarAristaConPeso(3, 5, 13);
+		miGrafo.agregarAristaConPeso(3, 4, 9);
+		miGrafo.agregarAristaConPeso(4, 5, 10);
+		miGrafo.agregarAristaConPeso(5, 6, 3);
+		miGrafo.agregarAristaConPeso(6, 8, 5);
+		miGrafo.agregarAristaConPeso(6, 7, 1);
+		miGrafo.agregarAristaConPeso(7, 8, 6);
+
+		List<Arista> agm = miGrafo.kruskal();
+
+		for (Arista a : agm) {
+			System.out.println(a.getOrigen() + " ---> " + a.getDestino() + " de peso: " + a.getPeso());
+		}
+	}
+
+}
